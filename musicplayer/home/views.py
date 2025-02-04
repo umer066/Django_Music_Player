03e7103195fa2f55eaed0f83fd531721d3f8f6a2
Hammad ul Rehman <hammad2006rehman@gmail.com>
2025-02-04 23:40:03 +0500

    Adding css and js files in static

--- musicplayer/home/views.py
+++ musicplayer/home/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     
-    paginator = Paginator(Song.object.all(), 1)
+    paginator = Paginator(Song.objects.all(), 1)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {"page_obj" : page_obj}
