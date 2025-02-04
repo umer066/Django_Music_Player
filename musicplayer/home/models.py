281873ae3e36b2d249567825688ddf1974117f03
Hammad ul Rehman <hammad2006rehman@gmail.com>
2025-02-05 03:53:50 +0500

    models.py

--- musicplayer/home/models.py
+++ musicplayer/home/models.py
@@ -6,10 +6,10 @@ class Song(models.Model):
    
    title = models.CharField(max_length= 50)
    singer = models.CharField(max_length= 50)
-   image = models.ImageField(upload_to=" ", max_length=250)
-   audio_file = models.FileField(upload_to=" ", max_length=250 )
-   audio_link = models.CharField(max_length=200, blank=True, default= " ")
-   lyrics = models.TextField(blank=True, default= " ")
+   image = models.ImageField(upload_to="", max_length=250)
+   audio_file = models.FileField(upload_to="", max_length=250 )
+   audio_link = models.CharField(max_length=200, blank=True, default= "")
+   lyrics = models.TextField(blank=True, default= "")
    duration = models.CharField(max_length=20)
    # paginate_by = 10
 
